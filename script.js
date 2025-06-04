4a37eb77400b05d9a8c0dc308e93a429955507b4
hoangganhh05 <hoang45682@gmail.com>
2025-06-04 23:53:37 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentInput = ''
+
+function press(value){
+    currentInput += value;
+    document.getElementById('display').value = currentInput;
+}
+
+
+function calculate(value){
+    try{
+        currentInput = eval(currentInput).toString();
+        document.getElementById('display').value = currentInput;
+    }catch(e){
+        document.getElementById('display').value = "Lỗi";
+    }
+}
+
+
+function ClearDisplay(value){
+    currentInput = '';
+    document.getElementById('display').value = '';
+}
